a27af0df3257a19925654051fb792a1467b4a003
manikanta <saimanikantakarnati12@gmail.com>
2023-08-20 16:44:32 +0530

    Video 3 Namaste React

--- App.js
+++ App.js
@@ -1,59 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const container = ReactDOM.createRoot(document.getElementById('container'));
-/* If we have multiple children, then we need to write them in an array which gives array of children */
-
-
-  // <div id="parent">
-  //   <div id="child">
-  //     <h1>H1 is in nested div</h1>
-  //     <h2>H2 is in nested div</h2>
-  //   </div>
-  //   <div id="child2">
-  //     <h2>H2 is in nested div</h2>
-  //     <h1>H1 is in nested div</h1>
-  //    </div>
-  // </div>
-
-const parent = React.createElement(
-  'div',
-  {id: "parent"},
-  [React.createElement(
-    'div',
-    {id: "child"},
-    [
-      React.createElement(
-        'h1',
-        {},
-        'H1 is in nested div'
-      ),
-      React.createElement(
-        'h2',
-        {},
-        'H2 is in nested div'
-      )
-      ]
-  ),
-  React.createElement(
-    'div',
-    {id: "child"},
-    [
-      React.createElement(
-        'h1',
-        {},
-        'H1 is in nested div'
-      ),
-      React.createElement(
-        'h2',
-        {},
-        'H2 is in nested div'
-      )
-      ]
-  )
-]
+//React element
+const Title = () => (
+  <div>
+      <h1 className="heading">Hello World</h1>
+      <h1>Hello</h1>
+  </div>
 );
 
-console.log(parent);
-container.render(parent); //Returns an object
-/* If we have multiple children, then we need to write them in an array which gives array of children */
+// React functional component
+// 3 ways to write component in component
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    {Title()} 
+    <Title></Title>
+    <h1 className="heading">Hello functional component</h1>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log(HeadingComponent)
+root.render(<HeadingComponent />);
+
